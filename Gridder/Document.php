3eab1215d1a3b0b138b80e6a9d5512ae09abe089
bazo <martin@bazo.sk>
2012-07-14 22:25:30 +0200

    added base document and css

---
+++ Gridder/Document.php
@@ -0,0 +1,31 @@
+<?php
+namespace Gridder;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+/**
+ * Document
+ *
+ * @author Martin Bazik
+ * @ODM\MappedSuperClass
+ */
+abstract class Document extends \Nette\Object implements ArrayAccess
+{
+	public function offsetExists($offset)
+    {
+        return isset($this->$offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        throw new BadMethodCallException("Array access of class " . get_class($this) . " is read-only!");
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    public function offsetUnset($offset)
+    {
+        throw new BadMethodCallException("Array access of class " . get_class($this) . " is read-only!");
+    }
+}
