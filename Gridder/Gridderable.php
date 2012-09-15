ea7fd67d4582cc8bff2ec5463c51d96060278a3b
bazo <martin@bazo.sk>
2012-09-15 17:59:29 +0200

    added gridderable trait

---
+++ Gridder/Gridderable.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Gridderable
+ *
+ * @author Martin
+ */
+trait Gridderable
+{
+	public function offsetExists($offset)
+    {
+        return isset($this->$offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        throw new BadMethodCallException("Array access of class " . get_class($this) . " is read-only!");
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    public function offsetUnset($offset)
+    {
+        throw new BadMethodCallException("Array access of class " . get_class($this) . " is read-only!");
+    }
+}
