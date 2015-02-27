adc9aaa1470f47cbf4cad71b7e6a7d62669ad1b2
bazo <martin@bazo.sk>
2015-02-27 14:32:15 +0100

    fixed neo4jrowiterator

--- Gridder/Sources/Neo4j/Neo4jRowIterator.php
+++ Gridder/Sources/Neo4j/Neo4jRowIterator.php
@@ -8,10 +8,20 @@ namespace Gridder\Sources\Neo4j;
  */
 class Neo4jRowIterator extends \Bazo\Neo4jTools\Neo4jRowIterator
 {
+
 	public function current()
 	{
 		$node = $this->resultSet->current()->current();
-		$properties = array_merge(['id' => $node->getId()], $node->getProperties());
+
+		if ($node instanceof \Everyman\Neo4j\Node) {
+			$properties = array_merge(['id' => $node->getId()], $node->getProperties());
+		} else {
+			$row		 = $this->resultSet->current();
+			$properties	 = iterator_to_array($row);
+		}
+
 		return $properties;
 	}
+
+
 }
